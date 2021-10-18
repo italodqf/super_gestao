851932ce6ebbcdc778cc96368a181a16842f28a9
devsquad-italo-felix <italo.felix@devsquad.com>
2021-10-18 13:58:41 -0300

    config routs

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', '\App\Http\Controllers\MainControllerclass@main');
+
+Route::get('/contact', '\App\Http\Controller\ContactController@contact');
+
+Route::get('/about-us', '\App\Http\Controller\AboutUsController@aboutus');
