923dc5457e049adb0204b831e929327133e19127
italodqf <italodouglasqf@gmail.com>
2021-10-19 09:44:25 -0300

    create menu

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -1,1 +1,6 @@
 <h1>Contact</h1>
+<ul>
+  <li><a href="/">Main</a></li>
+  <li><a href="contact">Contact</a></li>
+  <li><a href="about-us">Aboutus</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AboutUsController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use Illuminate\Validation\Rules\In;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,14 @@ use App\Http\Controllers\ContactController;
 */
 
 Route::get('/', [MainController::class, 'main']);
-
 Route::get('/contact', [ContactController::class, 'contact']);
-
 Route::get('/about-us', [AboutUsController::class, 'aboutus']);
+Route::get(
+    '/contact/{nome}{categoria_id}',
+    function (
+        string $nome = 'Desconhecido',
+        int $categoria_id = 1
+    ) {
+        echo "estamos aqui: $nome - $categoria_id";
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
